ee8f31280e0dd310b3ed81954b11b38bae74c7da
Andrea Johnson <ajohnson1@ajohnson-kcmo.vml.lan>
2019-02-22 11:08:24 -0600

    JS functions working

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@
 3. Add the numbers together
 4. Display 
 */
+
+function addNums(){
+    var numOne = parseFloat(document.getElementById("numberOne").value);
+    var numTwo = parseFloat(document.getElementById("numberTwo").value);
+    sum = numOne + numTwo;
+    return sum;
+};
+
+function displaySum(){
+    var output = addNums();
+    document.getElementById("numberOutput").innerHTML = output;
+};
